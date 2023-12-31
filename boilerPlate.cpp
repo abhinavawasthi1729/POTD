7d07852a7ef32ef5360dad55d6352049cb6d13d2
Abhinav Awasthi <abhinavawasthikvgn.com>
2024-01-01 01:45:09 +0530

    first commit

---
+++ boilerPlate.cpp
@@ -0,0 +1,15 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    freopen("E:\\coding practice\\newC++\\input.txt","r",stdin);
+    freopen("E:\\coding practice\\newC++\\output.txt","w",stdout);
+    int a=0,b=0;
+    cin>>a>>b;
+    cout<<"hi sum is "<<a+b;
+    
+  
+}
+    
